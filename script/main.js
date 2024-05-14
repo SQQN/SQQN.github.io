10c9b810f778450d0d18c79b7ddd869eacbe44dd
SQQN <42741492+SQQN@users.noreply.github.com>
2024-05-14 22:06:01 +0900

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,19 @@
+function Kitchen() {
+	document.location.href = "/kitchen.html"
+}
+
+function HookMore() {
+	document.location.href = "/hook.html"
+}
+
+function ShelfMore() {
+	document.location.href = "/shelf.html"
+}
+
+function SodaMore() {
+	document.location.href = "/soda.html"
+}
+
+function Question() {
+	document.location.href = "/qna.html"
+}
